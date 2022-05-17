4db77f04593b012d7c27481756571d0ab3d9d12b
eugene-sev <73117283+eugene-sev@users.noreply.github.com>
2022-05-17 18:15:22 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
+import shutil
+import datetime
+d = datetime.date.today()
+f = open ('m.txt', 'w')
+f.write (str(d)+'\n')
+f.close()
 def gam (name):
     unsv = 'y'
+    c = 0
     f = open ('m.txt', 'a')
     f.write (name +'\n')
     print ("-----------"+ name +"--------"+'\n')
     while unsv == 'y':
         a = input("name - ")
-        b = input("money - ")
+        try:
+            b = eval(input("money - "))
+        except NameError:
+            print('enter count (number)')
+            b = eval(input("money - "))
+        c += b
         f.write (a + " - " + str(b)+'\n')
         unsv = input("next? (y / n)")
-    f.write ("-----------"+ name + "---end------"+'\n')
+    f.write ("SUM "+ name +'\n' + str(c) +'\n' + "---------"+'\n')
     f.close()
-
 gam("debet")
 gam("credit")
+
+my_file = open('m.txt', 'r')
+tio = my_file.readlines()
+index = tio.index("SUM debet\n")
+index2 = tio.index("SUM credit\n")
+full = int(tio[index+1]) - int(tio[index2+1])
+f = open ('m.txt', 'a')
+f.write ("your many now is - " + str(full))
+f.close()
+shutil.copy('m.txt',str(d)+'.txt')
+print ("your many now is - " + str(full))
